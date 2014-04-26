a63d2694097e5ecf33f53c1483898e0561385efb
Vinnie Falco <vinnie.falco@gmail.com>
2014-04-25 17:44:20 -0700

    Improved SConstruct: * Automatic source list built via directory iteration * Build multiple toolchains and flavors simulaneously:     - Toolchains: gcc, clang, msvc     - Flavors: debug, release * Documentation on aliases (top of the SConstruct file)

--- src/ripple/proto/ripple_proto.unity.cpp
+++ src/ripple/proto/ripple_proto.unity.cpp
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 /*
     This file is part of rippled: https://github.com/ripple/rippled
-    Copyright (c) 2012, 2013 Ripple Labs Inc.
+    Copyright (c) 2014 Ripple Labs Inc.
 
     Permission to use, copy, modify, and/or distribute this software for any
     purpose  with  or without fee is hereby granted, provided that the above
@@ -17,11 +17,4 @@
 */
 //==============================================================================
 
-#if ! BEAST_COMPILE_OBJECTIVE_CPP
-#error Incorrect compilation setting!
-#endif
-
-#undef BEAST_COMPILE_OBJECTIVE_CPP
-#define BEAST_COMPILE_OBJECTIVE_CPP 0
-
-#include "ripple_beast.unity.cpp"
+#include "ripple.pb.cc"
